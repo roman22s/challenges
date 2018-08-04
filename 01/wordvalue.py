8aae8029e39034b73e57c83b703a6fad5a52d054
roman22s <roman22gilbert@yahoo.com>
2018-08-03 21:45:17 -0500

    Finisehd but better

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -3,31 +3,20 @@ from data import DICTIONARY, LETTER_SCORES
 
 def load_words():
     """Load dictionary into a list and return list"""
-    scrab_words = open(DICTIONARY, 'r')
-    words = [x.strip() for x in scrab_words.readlines()]
-    scrab_words.close()
-    return words
+    with open(DICTIONARY) as f:
+        return [word.strip() for word in f.read().split()]
 
 
 def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    score = 0
-    for letter in word.upper():
-        score += LETTER_SCORES.get(letter, None) or 0
-    return score
+    return sum(LETTER_SCORES.get(c.upper(), 0) for c in word)
 
 
-def max_word_value(words=load_words()):
+def max_word_value(words=None):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    hi_score = 0
-    for word in words:
-        score = calc_word_value(word)
-        if score > hi_score:
-            hi_score = score
-            hi_word = word
-    return hi_word
+    return max(words or load_words(), key=lambda w: calc_word_value(w))
 
 if __name__ == "__main__":
     pass  # run unittests to validate
